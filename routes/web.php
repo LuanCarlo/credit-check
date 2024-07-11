7bcce89abe114020805e5e7b2dc03b1b817c22e1
luancarlopaulo <luan.paulo@bigbangcomunicacao.com.br>
2024-07-11 02:28:00 -0300

    alterada rota inicial

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\CheckCreditOffersController::class, 'simulateCreditOfferPage']);
 
 Route::get('/simulate-credit-offer', [App\Http\Controllers\CheckCreditOffersController::class, 'simulateCreditOfferPage']);
 Route::get('/reports-credit-offer', [App\Http\Controllers\CheckCreditOffersController::class, 'reportCreditOfferPage']);
